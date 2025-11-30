e68ccb15dc58c9a7e7b64ed9423ef0091f1bae80
PeterMcD1 <peter.mcdevitt@verizon.net>
2025-11-29 23:19:12 -0700

    Initial commit: Flash Explorer MVP with FLTK and CMake

---
+++ File Explorer ++.cpp
@@ -0,0 +1,12 @@
+﻿// File Explorer ++.cpp : Defines the entry point for the application.
+//
+
+#include "File Explorer ++.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
